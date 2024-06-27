c82eda6735f411325184b4a655993cd1eaa04c35
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-27 17:26:15 +0800

    Updated Azure credential setup scripts for WebLogic VM and AKS services. Fixed issues with retrieving subscription ID and service principal ID.

--- .github/resource/azure-credential-setup-wls-aks.sh
+++ .github/resource/azure-credential-setup-wls-aks.sh
@@ -5,12 +5,14 @@ set -Eeuo pipefail
 echo "Execute azure-credential-setup.sh - Start------------------------------------------"
 
 ## Create Azure Credentials
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
 SERVICE_PRINCIPAL_NAME_WLS_AKS="sp-${REPO_NAME}-wls-aks-$(date +%s)"
 echo "Creating Azure Service Principal with name: $SERVICE_PRINCIPAL_NAME_WLS_AKS"
 SUBSCRIPTION_ID=$(az account show --query id -o tsv| tr -d '\r\n')
+echo "SUBSCRIPTION_ID="$SUBSCRIPTION_ID
 
 AZURE_CREDENTIALS=$(az ad sp create-for-rbac --name ${SERVICE_PRINCIPAL_NAME_WLS_AKS} --role="Contributor" --scopes="/subscriptions/${SUBSCRIPTION_ID}" --sdk-auth --only-show-errors)
-SP_ID=$( az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query \[0\].id -o tsv | tr -d '\r\n')
+SP_ID=$( az ad sp list --display-name $SERVICE_PRINCIPAL_NAME_WLS_AKS --query \[0\].id -o tsv | tr -d '\r\n')
 az role assignment create --assignee ${SP_ID} --scope="/subscriptions/${SUBSCRIPTION_ID}" --role "User Access Administrator"
 
 ## Set the Azure Credentials as a secret in the repository
--- .github/resource/azure-credential-setup-wls-vm.sh
+++ .github/resource/azure-credential-setup-wls-vm.sh
@@ -5,12 +5,13 @@ set -Eeuo pipefail
 echo "Execute azure-credential-setup.sh - Start------------------------------------------"
 
 ## Create Azure Credentials
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+
 SERVICE_PRINCIPAL_NAME_WLS_VM="sp-${REPO_NAME}-$(date +%s)"
 echo "Creating Azure Service Principal with name: $SERVICE_PRINCIPAL_NAME_WLS_VM"
 SUBSCRIPTION_ID=$(az account show --query id -o tsv| tr -d '\r\n')
-
-SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --name ${SERVICE_PRINCIPAL_NAME_WLS_VM} --role="Contributor" --scopes="/subscriptions/${SUBSCRIPTION_ID}" --sdk-auth --only-show-errors | base64 ${w0})
-AZURE_CREDENTIALS=$(echo $SERVICE_PRINCIPAL | base64 -d)
+echo "SUBSCRIPTION_ID="$SUBSCRIPTION_ID
+AZURE_CREDENTIALS=$(az ad sp create-for-rbac --name ${SERVICE_PRINCIPAL_NAME_WLS_VM} --role="Contributor" --scopes="/subscriptions/${SUBSCRIPTION_ID}" --sdk-auth --only-show-errors)
 
 ## Set the Azure Credentials as a secret in the repository
 gh secret set "AZURE_CREDENTIALS" -b"${AZURE_CREDENTIALS}"
