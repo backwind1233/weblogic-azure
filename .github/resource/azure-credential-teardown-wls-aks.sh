4b790bc3c179e404ac140ec87039c2c8119c7635
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-28 10:57:05 +0800

    Updated credential teardown script to delete correct service principal name for WLS AKS in Azure.

--- .github/resource/azure-credential-teardown-wls-aks.sh
+++ .github/resource/azure-credential-teardown-wls-aks.sh
@@ -7,5 +7,5 @@ echo "Execute azure-credential-teardown.sh - Start------------------------------
 gh secret delete "AZURE_CREDENTIALS"
 SERVICE_PRINCIPAL_NAME_WLS_AKS=$(gh variable get "SERVICE_PRINCIPAL_NAME_WLS_AKS")
 az ad app delete --id $(az ad sp list --display-name "$SERVICE_PRINCIPAL_NAME_WLS_AKS" --query '[].appId' -o tsv| tr -d '\r\n')
-gh variable delete "SERVICE_PRINCIPAL_NAME_WLS_VM"
+gh variable delete "SERVICE_PRINCIPAL_NAME_WLS_AKS"
 echo "Execute azure-credential-teardown.sh - End--------------------------------------------"
